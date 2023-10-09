5eb76c03aaf5e2c52508c427a3b5f8d09a1ab364
Karthik <130890917+karth1kkk@users.noreply.github.com>
2023-10-09 20:21:14 +0800

    Update Home.jsx

--- orion-AI/src/pages/Home.jsx
+++ orion-AI/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             setLoading(true)
 
             try{
-            const response = await fetch('http://localhost:8080/api/v1/post',{
+            const response = await fetch('https://vercel.com/karth1kkk/orion-ai/3SnrjsrtQG36m33AsZLw9zoXYFxi/api/v1/post',{
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
