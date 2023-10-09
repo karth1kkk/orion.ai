7aba1c0aa0ed0a09c501055c6f59a78391732dd1
Karthik <130890917+karth1kkk@users.noreply.github.com>
2023-10-09 21:36:50 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import orionRoutes from './routes/orionRoutes.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({ origin: 'https://orion-ai-dev.vercel.app' }));
 app.use(express.json({limit: '50mb'}))
 
 app.use('/api/v1/post', postRoutes)
